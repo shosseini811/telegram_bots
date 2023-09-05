f177e56f8083c6d77619175019dee21dc895ee0b
Sohail Hosseini <S.SADATHOSSEINI@GMAIL.COM>
2023-09-04 21:30:00 -0400

    bash script updated

--- youtube/simple_bots/update_script.sh
+++ youtube/simple_bots/update_script.sh
@@ -9,4 +9,7 @@ fi
 pkill -f youtubedl_bot.py
 
 # Start the updated script
-nohup python3 ~/youtubedl_bot.py &
+nohup python3 ~/youtubedl_bot.py > /dev/null 2>&1 < /dev/null &
+
+# Exit gracefully
+exit 0
