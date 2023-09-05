3e3ff1b2681c3862865f87b479c0d4bd14343243
Sohail Hosseini <S.SADATHOSSEINI@GMAIL.COM>
2023-09-04 21:23:01 -0400

    yaml filed updated to only install libraries if there is a change in requirements.txt

--- youtube/simple_bots/update_script.sh
+++ youtube/simple_bots/update_script.sh
@@ -1,15 +1,12 @@
 #!/bin/bash
 
-# Activate virtual environment (if you have one)
-# source /path/to/your/env/bin/activate
-
-# Install new requirements
-pip install -r ~/requirements.txt
+# If requirements.txt changed, then install new requirements
+if [ "$REQ_CHANGED" -eq "1" ]; then
+    pip install -r ~/requirements.txt
+fi
 
 # Kill the currently running script
 pkill -f youtubedl_bot.py
 
 # Start the updated script
 nohup python3 ~/youtubedl_bot.py &
-
-# Note: The 'nohup' command lets you run the script in the background and it will keep running even after you've closed the SSH connection.
