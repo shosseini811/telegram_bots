6311e79ae2219402425b83fbf3705ea3e4e3afc7
Sohail Hosseini <S.SADATHOSSEINI@GMAIL.COM>
2023-09-04 21:12:51 -0400

    bash file updated

--- youtube/simple_bots/update_script.sh
+++ youtube/simple_bots/update_script.sh
@@ -10,6 +10,6 @@ pip install -r ~/requirements.txt
 pkill -f youtubedl_bot.py
 
 # Start the updated script
-nohup python ~/youtubedl_bot.py &
+nohup python3 ~/youtubedl_bot.py &
 
 # Note: The 'nohup' command lets you run the script in the background and it will keep running even after you've closed the SSH connection.
