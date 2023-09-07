76035da1ab2d885171378b7edf0d6f6591dfcf12
Sohail Hosseini <S.SADATHOSSEINI@GMAIL.COM>
2023-09-07 18:56:08 -0400

    updated code to save files in downloads folder

--- spotify_dl/spotify_bot.py
+++ spotify_dl/spotify_bot.py
@@ -44,17 +44,26 @@ def send_welcome(message):
     logger.info(f"User {message.from_user.username} (ID: {message.from_user.id}) started the bot.")
 
 def get_most_recently_downloaded_file():
-    files = [f for f in os.listdir('.') if os.path.isfile(f) and f.endswith('.mp3')]
-    files.sort(key=lambda x: os.path.getmtime(x), reverse=True)
-    return files[0] if files else None
+    downloads_directory = os.path.expanduser('./downloads')
+    files = [f for f in os.listdir(downloads_directory) if os.path.isfile(os.path.join(downloads_directory, f)) and f.endswith('.mp3')]
+    files.sort(key=lambda x: os.path.getmtime(os.path.join(downloads_directory, x)), reverse=True)
+    return os.path.join(downloads_directory, files[0]) if files else None
 
-def extract_song_details_from_output(output):
-    # Extract singer and song name
-    match = re.search(r'Skipping (.*?) - (.*?) \(file already exists\)', output)
+
+def extract_song_details_from_output_v2(output):
+    # For a freshly downloaded song
+    match = re.search(r'Downloaded "(.*?) - (.*?) - (.*?)":', output)
     if match:
         singer = match.group(1)
-        song_name = match.group(2).replace(' ', '_') + '.mp3'
-        return singer, song_name
+        song_name = match.group(2)
+        additional_info = match.group(3)
+        return singer, f"{singer} - {song_name} - {additional_info}.mp3"
+
+    # For a duplicate song
+    match = re.search(r'Removing duplicate file: (.*?\.mp3)', output)
+    if match:
+        return None, match.group(1)
+    
     return None, None
 
 # Shared variable
@@ -69,9 +78,6 @@ def send_periodic_message(chat_id):
         bot.send_message(chat_id, "Please be patient, your song is being processed...")
         stop_event.wait(5)  # Wait for 5 seconds or until the event is set
 
-
-
-
 @bot.message_handler(func=lambda message: True)
 def handle_song_link(message):
     global should_continue_sending_messages
@@ -80,20 +86,27 @@ def handle_song_link(message):
     
     # Start the periodic message thread
     threading.Thread(target=send_periodic_message, args=(message.chat.id,)).start()
+    
     # Create a new connection and cursor for this thread
     conn_local = sqlite3.connect('songs.db')
     cursor_local = conn_local.cursor()
 
     link = message.text
+    
+    # Save the current directory and switch to the 'Downloads' directory
+    original_directory = os.getcwd()
+    downloads_directory = os.path.expanduser('./downloads')
+    if not os.path.exists(downloads_directory):
+        os.makedirs(downloads_directory)
+    os.chdir(downloads_directory)
+
     result = os.popen(f'python -m spotdl {link}').read()
-    singer, song_path = extract_song_details_from_output(result)
-    if not singer or not song_path:
-        song_path = get_most_recently_downloaded_file()
-    else:
-        # Create directory based on singer's name
-        if not os.path.exists(singer):
-            os.makedirs(singer)
-        song_path = os.path.join(singer, song_path)
+    
+    # Switch back to the original directory
+    os.chdir(original_directory)
+
+    singer, _ = extract_song_details_from_output_v2(result)
+    song_path = get_most_recently_downloaded_file()
 
     if not song_path or not os.path.exists(song_path):
         bot.reply_to(message, "Sorry, couldn't find the downloaded song.")
@@ -101,7 +114,7 @@ def handle_song_link(message):
 
     # Send the song back to the user
     with open(song_path, 'rb') as song_file:
-        bot.send_audio(message.chat.id, song_file, caption=f"Here's your song: {song_path}")
+        bot.send_audio(message.chat.id, song_file, caption=f"Here's your song: {os.path.basename(song_path)}")
 
     # Log user information and the posted link
     logger.info(f"User {message.from_user.username} (ID: {message.from_user.id}) posted link: {link}")
@@ -109,13 +122,12 @@ def handle_song_link(message):
     # Save song details to the SQLite database
     current_timestamp = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
     cursor_local.execute("INSERT INTO songs (user_id, user_name, song_name, timestamp) VALUES (?, ?, ?, ?)", 
-                (message.from_user.id, message.from_user.username, song_path, current_timestamp))
+                (message.from_user.id, message.from_user.username, os.path.basename(song_path), current_timestamp))
     conn_local.commit()
     conn_local.close()  # Close the connection
 
-    # # Stop sending periodic messages
-    # should_continue_sending_messages = False
     # Stop sending periodic messages
     stop_event.set()
+
 
 bot.polling(none_stop=True)
